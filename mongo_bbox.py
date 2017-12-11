0807366de2059d7c6290ed4ed6f3c0a188fabe9f
Petr Silhak <petrsilhak@gmail.com>
2017-12-11 19:28:06 +0100

    Added mongodb with bbox and time

--- mongo_bbox.py
+++ mongo_bbox.py
@@ -1,1 +1,31 @@
 from pymongo import MongoClient
+import csv, time
+
+client = MongoClient()
+db = client.phd
+
+f = open('mongodb_bbox_time.csv', 'w')
+with open('bboxs.csv') as file:
+    reader = csv.reader(file, delimiter='\t')
+    for row in reader:
+        start = time.time()
+
+        xmin, ymin, xmax, ymax = row[1].replace('SELECT * FROM roads WHERE Intersects(BuildMbr(','').replace('), geom);','').split(', ')
+        query = {'geometry':
+            {'$geoIntersects':
+                {'$geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [
+                        [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax], [xmin, ymin]]
+                        ]
+                    }
+                }
+            }
+        }
+        db.roads.find(query)
+
+        end = time.time()
+        diff = end - start
+        f.write(str(query) + '\t' + str('{0:.10f}'.format(diff)) + '\n')
+
+f.close()
